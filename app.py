757484bbee860beffd86f54203eb22775bce66c0
asminpandit <161024454+asminpandit@users.noreply.github.com>
2024-05-02 01:57:01 +0530

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template
+from gunicorn.app.base import BaseApplication
+from your_flask_app import app
 
-app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+class StandaloneApplication(BaseApplication):
+    def __init__(self, app, options=None):
+        self.options = options or {}
+        self.application = app
+        super(StandaloneApplication, self).__init__()
+
+    def load_config(self):
+        config = {key: value for key, value in self.options.items() if key in self.cfg.settings and value is not None}
+        for key, value in config.items():
+            self.cfg.set(key.lower(), value)
+
+    def load(self):
+        return self.application
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    options = {
+        'bind': '0.0.0.0:8080',  # or the port of your choice
+        'workers': 1,
+    }
+    StandaloneApplication(app, options).run()
+
